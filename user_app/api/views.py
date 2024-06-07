712a8f0d3e22397dca0f07ac03c0cb96b385866d
Om Dusane <omdusane8@gmail.com>
2024-06-07 09:50:00 +0530

    Added auto delete tokem flow

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from user_app.api.serializers import RegisterUserSerializer
+from rest_framework.authentication import TokenAuthentication
 
 @api_view(['POST'])
 def registration_view(request):
@@ -9,3 +10,11 @@ def registration_view(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+def logout_view(request):
+    if request.method == 'POST':
+        logout = request.user.auth_token.delete()
+        return Response({"message": "Logged out successfully!"})
+
